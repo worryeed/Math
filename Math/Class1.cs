fd88dce4a8e94d32caf2a516f88653f34b1a2b05
worryeed <lahov.sasha525@yandex.ru>
2024-04-20 23:01:17 +0300

    Добавлен класс MyMath

--- Math/Class1.cs
+++ Math/Class1.cs
@@ -1,7 +1,17 @@
 ﻿namespace Math
 {
-    public class Class1
+    public class MyMath
     {
+        public int Sum(int a, int b) => a + b;
+        public double Sum(double a, double b) => a + b;
 
+        public int Subtraction(int a, int b) => a - b;
+        public double Subtraction(double a, double b) => a - b;
+
+        public int Multiply(int a, int b) => a * b;
+        public double Multiply(double a, double b) => a * b;
+
+        public int Division(int a, int b) => b == 0 ? throw new DivideByZeroException() : a / b;
+        public double Division(double a, double b) => b == 0 ? throw new DivideByZeroException() : a / b;
     }
 }
